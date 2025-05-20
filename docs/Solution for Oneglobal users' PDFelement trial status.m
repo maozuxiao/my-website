aa839d7a01a0f7ac33a0f2ea9420888f45886382
maozuxiao <48650555+maozuxiao@users.noreply.github.com>
2025-05-20 11:26:45 +0800

    Create Solution for Oneglobal users' PDFelement trial status.m

---
+++ docs/Solution for Oneglobal users' PDFelement trial status.m
@@ -0,0 +1,24 @@
+# Solution for Oneglobal users' PDFelement trial status
+
+## ①. Check the PDFelement version
+
+Once you have opened the program, click on the **three-dot icon** in the top right corner to access more settings. Then click the **About** option.
+
+![About Button](https://images.wondershare.com/pdfelement/guide-mac/guide-mac2022/guide-win2022/about-pdfelement.png)
+
+The About window will be opened and show you the current version information of PDFelement.
+
+<img src="https://cdn.jsdelivr.net/gh/maozuxiao/Misaki/20250520111546900.png" alt="Version Info" style="zoom:50%;" />
+
+If your version is not **PDFelement Business Version: 10.4.15**,please download the correct installer from the below link.
+
+:link:https://download.wondershare.com/business/prd/PDFelement_Business_WSID_AI_Cloud_esign_2870_source_5501_10.4.15.exe
+
+## ②. Check the license status
+
+1. Login the [Account Center](https://accounts.wondershare.com/web/products)
+2. Navigate to Plan & Products>>Manage Products
+3. Check if assigned the "**Wondershare PDFelement Business for Windows**" License
+   ![Manage Product](https://cdn.jsdelivr.net/gh/maozuxiao/Misaki/20250520112103298.png)
+
+PS: If you didn't find the license under your account, please contact lywee@ogbroking.com to assign you a license
